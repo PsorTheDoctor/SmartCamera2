01af7ce83c2b73367f2db80cbc77a48672f039c5
PsorTheDoctor <psor2.0@gmail.com>
2021-12-06 23:41:37 +0100

    Initial Commit

---
+++ SmartCamera2/SmartCamera2App.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCamera2App.swift
+//  SmartCamera2
+//
+//  Created by USER on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SmartCamera2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
