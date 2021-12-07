46a38d473cc137c5fbbaf472ca7cb1464183d5f7
PsorTheDoctor <psor2.0@gmail.com>
2021-12-07 02:05:19 +0100

    Initial commit

--- SmartCamera2/ContentView.swift
+++ SmartCamera2/ContentView.swift
@@ -2,10 +2,11 @@
 //  ContentView.swift
 //  SmartCamera2
 //
-//  Created by USER on 06/12/2021.
+//  Created by Adam Wolkowycki on 06/12/2021.
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
@@ -17,5 +18,118 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CameraView: View {
+    
+    @StateObject var camera = CameraModel()
+    
+    var body: some View {
+        ZStack {
+            CameraPreview(camera: camera).ignoresSafeArea(.all, edges: .all)
+            
+            VStack {
+                if camera.isTaken {
+                    HStack {
+                        Spacer()
+                        Button(action: camera.retake, label: {
+                            Image(systemName: "arrow.triangle.2.circlepath.camera")
+                                .foregroundColor(.black)
+                                .padding()
+                                .background(Color.white)
+                                .clipShape(Circle())
+                        }) .padding(.trailing, 10)
+                    }
+                } else {
+                    HStack {
+                        Button(action: camera.takePhoto, label: {
+                            ZStack {
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 65, height: 65)
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 2)
+                                    .frame(width: 70, height: 70)
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
+
+class CameraModel: NSObject, ObservableObject, AVCapturePhotoCaptureDelegate {
+    
+    @Published var isTaken = false
+    @Published var session = AVCaptureSession()
+    @Published var output = AVCapturePhotoOutput()
+    @Published var preview: AVCaptureVideoPreviewLayer!
+    
+    func setup() {
+        do {
+            self.session.beginConfiguration()
+            
+            let device = AVCaptureDevice.default(for: .video)
+            let input = try AVCaptureDeviceInput(device: device!)
+            
+            if self.session.canAddInput(input) {
+                self.session.addInput(input)
+            }
+            if self.session.canAddOutput(self.output) {
+                self.session.addOutput(self.output)
+            }
+            self.session.commitConfiguration()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func takePhoto() {
+        DispatchQueue.global(qos: .background).async {
+            self.output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+            self.session.stopRunning()
+            
+            DispatchQueue.main.async {
+                withAnimation { self.isTaken.toggle() }
+            }
+        }
+    }
+    
+    func retake() {
+        DispatchQueue.global(qos: .background).async {
+            self.session.startRunning()
+            
+            DispatchQueue.main.async {
+                withAnimation { self.isTaken.toggle() }
+            }
+        }
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if error != nil { return }
+    }
+}
+
+struct CameraPreview: UIViewRepresentable {
+    
+    @ObservedObject var camera: CameraModel
+    
+    func makeUIView(context: Context) -> UIView {
+        
+        let view = UIView(frame: UIScreen.main.bounds)
+        
+        camera.preview = AVCaptureVideoPreviewLayer(session: camera.session)
+        camera.preview.frame = view.frame
+        camera.preview.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(camera.preview)
+        camera.session.startRunning()
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
     }
 }
