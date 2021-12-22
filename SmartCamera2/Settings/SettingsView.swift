df0a75bd8a114a02cec42ef4269e08e5dcc20ee5
PsorTheDoctor <psor2.0@gmail.com>
2021-12-22 06:18:27 +0100

    rating added

--- SmartCamera2/Settings/SettingsView.swift
+++ SmartCamera2/Settings/SettingsView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
+    
+    let mint = Color(red: 0.2, green: 0.6, blue: 0.4)
     
     @State private var isShareSheetShowing = false
     
@@ -20,14 +23,45 @@ struct SettingsView: View {
         UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
     }
     
+    func rate() {
+        guard let scene = UIApplication.shared.windows.first?.windowScene else { return }
+        
+        SKStoreReviewController.requestReview(in: scene)
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
-                Button(action: share) {
-                    Text("Share")
-                    Image(systemName: "square.and.arrow.up")
-                        .font(.largeTitle)
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(mint)
+                            .frame(height: 100)
+                        Button(action: share) {
+                            Text("Share")
+                            Image(systemName: "square.and.arrow.up")
+                                .font(.largeTitle)
+                        }
+                        .background(mint)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    }
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(mint)
+                            .frame(height: 100)
+                        Button(action: rate) {
+                            Text("Rate")
+                            Image(systemName: "star")
+                                .font(.largeTitle)
+                        }
+                        .background(mint)
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                    }
                 }
+                Spacer()
             }.navigationTitle("Settings")
         }
     }
